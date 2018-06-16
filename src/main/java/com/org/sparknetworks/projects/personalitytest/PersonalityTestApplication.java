9ecd09e4d3a504ebb53e8ba5505cfa528f0a4b6f
RAHUL DESHMUKH <rahuldeshmukh28@gmail.com>
2018-06-17 01:15:50 +0200

    Intial Project Setup

---
+++ src/main/java/com/org/sparknetworks/projects/personalitytest/PersonalityTestApplication.java
@@ -0,0 +1,12 @@
+package com.org.sparknetworks.projects.personalitytest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalityTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalityTestApplication.class, args);
+	}
+}
---
+++ src/test/java/com/org/sparknetworks/projects/personalitytest/PersonalityTestApplicationTests.java
@@ -0,0 +1,16 @@
+package com.org.sparknetworks.projects.personalitytest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PersonalityTestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
