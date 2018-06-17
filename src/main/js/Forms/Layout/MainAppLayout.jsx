d95bb779ab3213cd54f088add475063efa6ce5ab
RAHUL DESHMUKH <rahuldeshmukh28@gmail.com>
2018-06-17 11:15:51 +0200

    Footer component and main layout for page.

---
+++ src/main/js/Forms/Layout/MainAppLayout.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './MainAppLayout.scss';
+
+const MainAppLayout = (props) => {
+    return (
+      <div>
+          <header>Header</header>
+          <nav>Navigation</nav>
+          <main>
+              main component
+          </main>
+          <aside>Aside</aside>
+          <footer>Footer</footer>
+      </div>
+    );
+}
+
