766b8ec187d65c7ab7ad36c11fd5449acccb1942
RAHUL DESHMUKH <rahuldeshmukh28@gmail.com>
2018-06-17 12:44:30 +0200

    Success and Error page setup.

--- src/main/js/App.jsx
+++ src/main/js/App.jsx
@@ -4,6 +4,8 @@ import Home from "./Forms/Home/Home.jsx";
 import MainAppLayout from "./Forms/Layout/MainAppLayout.jsx";
 import "./App.scss";
 import PersonalityTest from "./Forms/PersonalityTest/PersonalityTest.jsx";
+import SuccessPage from "./Forms/Utils/SuccessPage.jsx";
+import ErrorPage from "./Forms/Utils/ErrorPage";
 
 export default class App extends Component {
     constructor(props) {
@@ -17,6 +19,8 @@ export default class App extends Component {
                 <Route exact={true} path="/" render={() => <MainAppLayout> <Home/> </MainAppLayout>} />
                 <Route exact={true} path="/home" render={() => <MainAppLayout> <Home/> </MainAppLayout>} />
                 <Route exact={true} path="/home/personalityTest" render={() => <MainAppLayout> <PersonalityTest /> </MainAppLayout>} />
+                <Route exact={true} path="/success" render={ () => <MainAppLayout> <SuccessPage/> </MainAppLayout> } />
+                <Route exact={true} path="/error" render={ () => <MainAppLayout> <ErrorPage/> </MainAppLayout> } />
             </Switch>
 
         </div>);
