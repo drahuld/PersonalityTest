82b3c9793ff77a6483cc166ffc6400664da0c55d
RAHUL DESHMUKH <rahuldeshmukh28@gmail.com>
2018-06-17 12:43:47 +0200

    Success and Error page setup.

---
+++ src/main/js/Forms/Utils/ErrorPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const ErrorPage = (props) => {
+
+    return(
+        <div>
+
+            <h1 style={{color: 'RED'}}>Some error occurred while saving your answers. Please resubmit again !!! </h1>
+
+        </div>
+    );
+};
+
+export default ErrorPage;
---
+++ src/main/js/Forms/Utils/SuccessPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Success = (props) => {
+
+    return(
+        <div>
+
+            <h1 style={{color: 'Green'}}>Thank you ! Your answers are successfully saved !!! </h1>
+
+        </div>
+    );
+};
+
+export default Success;
